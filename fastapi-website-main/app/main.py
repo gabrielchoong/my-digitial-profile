5b2c7bbf1821a16d7133f996c040ff8a980b54e0
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-02-08 12:23:22 +0800

    Fix import statements for helper and database modules

--- fastapi-website-main/app/main.py
+++ fastapi-website-main/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from app.schemas import Student
 
-from helper import save_data, load_data
-from database import db
+from .helper import save_data, load_data
+from .database import db
 
 app = FastAPI()
 
